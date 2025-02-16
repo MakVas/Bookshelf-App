56ffb1153b264f66a8b5092d559b1a7aeb153622
makvas <andriymvnaruto@gmail.com>
2025-02-16 21:41:14 +0200

    Refactored stateUtils

---
+++ app/src/main/java/com/makvas/bookshelfapp/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,24 @@
+package com.makvas.bookshelfapp.ui.utils
+
+sealed interface Response {
+    data class Success<T>(val data: T) : Response
+    data object Loading : Response
+    data object Error : Response
+}
+
+enum class ScreenType {
+    START,
+    HOME,
+    DETAILS
+}
+
+enum class TopAppBarType {
+    DEFAULT,
+    SEARCH,
+    DETAILS
+}
+
+enum class ContentType {
+    LIST_ONLY,
+    LIST_AND_DETAIL
+}
